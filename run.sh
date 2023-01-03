dcb91c61a3c45247dcfb2ad43c66d1213c8cf79e
Tran Xuan Loc <loc.tran@moreh.com.vn>
2023-01-03 13:48:12 +0900

    change explicitly for A100 VM

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# somehow tensorflow cannot find the right cuda & cudnn lib to load at runtime,
-# so set some env vars to correct it
-export PATH=/usr/local/cuda-11.2/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-11.2/lib64:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-11.2/include:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-11.2/extras/CUPTI/lib64
+if [[ $HOSTNAME =~ ^haca100[0-9] ]]; then
+    # on A100 VM, somehow tensorflow cannot find the right cuda & cudnn lib to load
+    # at runtime, so set some env vars to correct it
+    export PATH=/usr/local/cuda-11.2/bin${PATH:+:${PATH}}
+    export LD_LIBRARY_PATH=/usr/local/cuda-11.2/lib64:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=/usr/local/cuda-11.2/include:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-11.2/extras/CUPTI/lib64
+fi
 
-python ddim.py
+/usr/bin/env python3 ddim.py
